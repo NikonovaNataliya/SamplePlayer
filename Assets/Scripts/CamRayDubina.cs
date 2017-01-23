ab7633c122b398c5cee27a94db133aaead36681a
Nikonova Nataliya <natasha.nickonova@gmail.com>
2017-01-23 19:42:32 +0200

    problem: layer animation

---
+++ Assets/Scripts/CamRayDubina.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamRayDubina : MonoBehaviour {
+
+    public enum InversionX { Disabled = 0, Enabled = 1 };
+    public enum InversionY { Disabled = 0, Enabled = 1 };
+    public enum Smooth { Disabled = 0, Enabled = 1 };
+
+    [Header("General")]
+    public float sensitivity = 5f;
+    public float distanse = 5f;
+    public float height = 2.3f;
+
+    [Header("Over The Shoulder")]
+    public float offsetPosition;
+
+    [Header("Clamp Angle")]
+    public float minY = 0f;
+    public float maxY = 0f;
+
+    [Header("Invert")]
+    public InversionX inversionX = InversionX.Disabled;
+    public InversionY inversionY = InversionY.Disabled;
+
+    [Header("Smooth Movement")]
+    public Smooth smooth;
+    public float speed = 8f;
+
+    private float rotationY;
+    private int inversY, inversX;
+    private Transform player;
+
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        gameObject.tag = "MainCamera";
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+    Vector3 PositionCorrection(Vector3 target, Vector3 position) { 
+            RaycastHit hit;
+        Debug.DrawLine(target, position, Color.blue);
+        if (Physics.Linecast(target,position,out hit)) {
+            float TempDistance = Vector3.Distance(target, hit.point);
+            Vector3 pos = target - (transform.rotation * Vector3.forward * TempDistance);
+            position = new Vector3(pos.x, position.y, pos.z);
+        }
+            return position;
+
+        }
+
+
+    void LateUpdate() {
+
+        if (player) {
+            if (inversionX == InversionX.Disabled) inversX = 1; else inversX = -1;
+            if (inversionY == InversionY.Disabled) inversY = -1; else inversY = 1;
+
+            transform.RotateAround(transform.position, Vector3.up, Input.GetAxis("Mouse X") * sensitivity * inversX);
+            
+            Vector3 position = player.position - (transform.rotation * Vector3.forward * distanse);
+            position = position + (transform.rotation * Vector3.right * offsetPosition);
+            position = new Vector3(position.x, player.position.y + height, position.z);
+            position = PositionCorrection(player.position, position);
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivity;
+            rotationY = Mathf.Clamp(rotationY, -Mathf.Abs(minY), Mathf.Abs(maxY));
+            transform.localEulerAngles = new Vector3(rotationY*inversY, transform.localEulerAngles.y, 0);
+
+            if (smooth == Smooth.Disabled) transform.position = position;
+            else transform.position = Vector3.Lerp(transform.position, position, speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/xxxMoving.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class xxxMoving : MonoBehaviour {
+
+    CharacterController controller;
+    Animator anim;
+
+    float h, v;
+    Vector3 direction;
+    public float speed, speedSideward, speedRun, speedRot;
+  
+	void Start () {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+	}
+
+    void FixedUpdate() {
+        Move();
+        GeroyAnimation();
+    }
+
+    void Update () {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+	}
+
+    void Move() {
+        //вектор направления движения
+        direction = new Vector3(h, 0, v);
+        direction = Camera.main.transform.TransformDirection(direction);
+        direction = new Vector3(direction.x, 0, direction.z);
+        //разворот в сторону движения
+        if (v > 0) {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(direction),
+                                                                                 speedRot * Time.deltaTime);
+        }
+        if (v < 0) {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.FromToRotation(Vector3.back, direction),
+                                                                                             speedRot * Time.deltaTime);
+        }
+        if (h > 0 && v == 0) {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.FromToRotation(Vector3.right, direction),
+                                                                                             speedRot * Time.deltaTime);
+        }
+        if (h < 0 && v == 0) {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.FromToRotation(Vector3.left, direction),
+                                                                                             speedRot * Time.deltaTime);
+        }
+        if ((v != 0 && h == 0) || (v != 0 && h != 0))
+            controller.Move(direction * speed * Time.deltaTime);
+        if (h != 0 && v == 0)
+            controller.Move(direction * speedSideward * Time.deltaTime);
+        if (Input.GetKey(KeyCode.X))
+            controller.Move(direction * speedRun * Time.deltaTime);
+    }
+
+    void GeroyAnimation() {
+        
+        anim.SetFloat("Vertical", v, 0.01f, Time.fixedDeltaTime);
+        anim.SetFloat("Horizontal", h, 0.01f, Time.fixedDeltaTime);
+
+        if (v == 0 && h != 0) {
+            anim.SetBool("Sideward", true);
+            anim.SetBool("Move", false);
+        }
+        if (v != 0 && h == 0) {
+            anim.SetBool("Sideward", false);
+            anim.SetBool("Move", true);
+        }
+        if (v == 0 && h == 0) {
+            anim.SetBool("Sideward", false);
+            anim.SetBool("Move", false);
+        }
+        if (v != 0 && h != 0) {
+            anim.SetBool("Sideward", false);
+            anim.SetBool("Move", true);
+        }
+
+        if (Input.GetMouseButton(0)) {
+            anim.SetBool("Attack", true);
+        }
+        else anim.SetBool("Attack", false);
+
+        if (Input.GetKey(KeyCode.X)) {
+            anim.SetBool("Run", true);
+        }
+        else anim.SetBool("Run", false);
+    }
+}
